439cd566ca5c3034c2d59ba47b7f951bac7cf5ca
prashantshekhar <prashant.shekhar2541@gmail.com>
2025-11-26 01:57:10 +0530

    exploring get, post and delete and order

--- src/app.js
+++ src/app.js
@@ -7,9 +7,25 @@ const app = express();
 app.use('/test' , (req, res) => {
     res.send('hello , how are you');
 })
-
+// doing app.use will match all the http method api calls to /home/users 
 app.use('/home', (req, res) => {
     res.send('hello from the server');
+})
+app.use('/user' , (req , res) => {
+    res.send("this is before all requset which will match all requset made on this url , because order matters ")
+})
+
+// but this route (app.get) will match the GET API  call 
+app.get('/user' , (req , res) => {
+    res.send({name : 'prashant' , lastname : 'shekhar'});
+});
+
+app.post('/user', (req , res) => {
+    res.send("Data successfully saved to the database!");
+})
+
+app.delete('/user' , (req , res) => {
+    res.send("deleted the data successfully from the database");
 })
 app.listen(3000 , () => {
     console.log('server has created sussessfully');
